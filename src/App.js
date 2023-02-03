04746d29eadc9b874af876039e84be77f2a3ac97
Sean Warman <seanwarman@gmail.com>
2023-02-03 21:12:39 +0000

    App to expose event handler, axios config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
 
 import Header from './components/Header'
 import Main from './components/Main'
@@ -7,18 +8,43 @@ import Repos from './components/Repos'
 
 import { GIT_RENDER_INITIAL_SEARCH } from './constants'
 
-import { repo } from './stubs/githubSearchApi'
+async function getData(search) {
+  try {
+    const { data } = await axios(`https://api.github.com/search/repositories?${search.toString()}`) 
+    const { items } = data
 
-function App() {
+    return items
+
+  } catch (error) {
+    throw error
+  }
+}
+
+function App({ onChangeSearch = getData }) {
   const [favourites, setFavourites] = useState([])
   const [search, setSearch] = useSearchParams()
   const [favFilter, setFavFilter] = useState(false)
+  const [repos, setRepos] = useState([])
 
   useEffect(() => {
     if (!search.toString().length) {
       setSearch(GIT_RENDER_INITIAL_SEARCH)
     }
   }, [search, setSearch])
+
+  useEffect(() => {
+    if (search.toString().length) {
+      (async () => {
+        try {
+          const items = await onChangeSearch(search)
+          setRepos(items)
+        } catch (error) {
+          console.log(`@FILTER error:`, error)
+          // setError(error)
+        }
+      })()
+    }
+  }, [onChangeSearch, search, setRepos])
 
   return (
     <>
@@ -30,7 +56,7 @@ function App() {
         <Repos
           favFilter={favFilter}
           favourites={favourites}
-          repos={Array(9).fill(repo).map((r,i) => ({ ...r, id: r.id + i }))}
+          repos={repos}
           onCheck={checkedId => {
             setFavourites(prev => {
               if (!prev.includes(checkedId)) return [...prev, checkedId]
